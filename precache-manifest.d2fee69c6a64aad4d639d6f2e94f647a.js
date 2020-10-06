813c98295e75976c380e01d9c9192ab75ca68bff
toby <toby.rahilly@gmail.com>
2020-10-06 21:09:27 +1100

    Updates

---
+++ precache-manifest.d2fee69c6a64aad4d639d6f2e94f647a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1765314c56acaa4b833b7c621bc4a9f",
+    "url": "/userDirectory/index.html"
+  },
+  {
+    "revision": "9f640f963762cb4f4eca",
+    "url": "/userDirectory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4894b5e34d3fb75553d1",
+    "url": "/userDirectory/static/css/main.b584eadd.chunk.css"
+  },
+  {
+    "revision": "9f640f963762cb4f4eca",
+    "url": "/userDirectory/static/js/2.02a0c0cb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/userDirectory/static/js/2.02a0c0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4894b5e34d3fb75553d1",
+    "url": "/userDirectory/static/js/main.6516bff5.chunk.js"
+  },
+  {
+    "revision": "8a6c1d8d2c0a9f9483bf",
+    "url": "/userDirectory/static/js/runtime-main.08cdabe6.js"
+  }
+]);
--- src/components/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Wrapper extends React.Component{
-    render(){
-        return(
-            <div className="wrapper">{this.props.children}</div>
-        );
-    }
-}
